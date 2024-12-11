141036666e8c4ee2d321d4c727b481f04265755b
Robert Fratto <robertfratto@gmail.com>
2024-12-11 13:50:38 -0500

    dataobj: dedupe code

--- pkg/dataobj/internal/decoder/scan.go
+++ pkg/dataobj/internal/decoder/scan.go
@@ -3,6 +3,7 @@ package decoder
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/grafana/loki/v3/pkg/dataobj/internal/filemd"
@@ -10,7 +11,7 @@ import (
 	"github.com/grafana/loki/v3/pkg/dataobj/internal/streamsmd"
 )
 
-// scanTailer decodes the tailer of the file to retrieve the metadata size and
+// scanTailer scans the tailer of the file to retrieve the metadata size and
 // the magic value.
 func scanTailer(s scanner.Scanner) (metadataSize uint32, err error) {
 	if err := binary.Read(s, binary.LittleEndian, &metadataSize); err != nil {
@@ -27,36 +28,23 @@ func scanTailer(s scanner.Scanner) (metadataSize uint32, err error) {
 	return
 }
 
-// scanFileMetadata decodes a set of Sections from s.
-func scanFileMetadata(s scanner.Scanner) (filemd.Metadata, error) {
+// scanFileMetadata scans file metadata from s.
+func scanFileMetadata(s scanner.Scanner) (*filemd.Metadata, error) {
 	fileFormatVersion, err := binary.ReadUvarint(s)
 	if err != nil {
-		return filemd.Metadata{}, fmt.Errorf("read file format version: %w", err)
+		return nil, fmt.Errorf("read file format version: %w", err)
 	} else if fileFormatVersion != 1 {
-		return filemd.Metadata{}, fmt.Errorf("unsupported file format version: %d", fileFormatVersion)
+		return nil, fmt.Errorf("unsupported file format version: %d", fileFormatVersion)
 	}
-
-	metadataSize, err := binary.ReadUvarint(s)
-	if err != nil {
-		return filemd.Metadata{}, fmt.Errorf("read metadata size: %w", err)
-	}
-
-	metadataBytes, err := s.Peek(int(metadataSize))
-	if err != nil {
-		return filemd.Metadata{}, fmt.Errorf("read file metadata: %w", err)
-	} else if len(metadataBytes) != int(metadataSize) {
-		return filemd.Metadata{}, fmt.Errorf("read file metadata: short read")
-	}
-	defer s.Discard(int(metadataSize))
 
 	var metadata filemd.Metadata
-	if err := proto.Unmarshal(metadataBytes, &metadata); err != nil {
-		return filemd.Metadata{}, fmt.Errorf("unmarshal file metadata: %w", err)
+	if err := scanProto(s, &metadata); err != nil {
+		return nil, fmt.Errorf("stream metadata: %w", err)
 	}
-	return metadata, nil
+	return &metadata, nil
 }
 
-// scanStreamsMetadata decodes a set of Streams from s.
+// scanStreamsMetadata scans streams section metadata from s.
 func scanStreamsMetadata(s scanner.Scanner) (*streamsmd.Metadata, error) {
 	formatVersion, err := binary.ReadUvarint(s)
 	if err != nil {
@@ -65,66 +53,46 @@ func scanStreamsMetadata(s scanner.Scanner) (*streamsmd.Metadata, error) {
 		return nil, fmt.Errorf("unsupported streams format version: %d", formatVersion)
 	}
 
-	metadataSize, err := binary.ReadUvarint(s)
-	if err != nil {
-		return nil, fmt.Errorf("read metadata size: %w", err)
-	}
-
-	metadataBytes, err := s.Peek(int(metadataSize))
-	if err != nil {
-		return nil, fmt.Errorf("read metadata: %w", err)
-	} else if len(metadataBytes) != int(metadataSize) {
-		return nil, fmt.Errorf("read metadata: short read")
-	}
-	defer s.Discard(int(metadataSize))
-
 	var metadata streamsmd.Metadata
-	if err := proto.Unmarshal(metadataBytes, &metadata); err != nil {
-		return nil, fmt.Errorf("unmarshal metadata: %w", err)
+	if err := scanProto(s, &metadata); err != nil {
+		return nil, fmt.Errorf("stream metadata: %w", err)
 	}
 	return &metadata, nil
 }
 
-// scanStreamMetadata decodes a set of Columns from s.
+// scanStreamMetadata scans a stream's metadata from s.
 func scanStreamMetadata(s scanner.Scanner) (*streamsmd.StreamMetadata, error) {
-	metadataSize, err := binary.ReadUvarint(s)
-	if err != nil {
-		return nil, fmt.Errorf("read metadata size: %w", err)
-	}
-
-	metadataBytes, err := s.Peek(int(metadataSize))
-	if err != nil {
-		return nil, fmt.Errorf("read metadata: %w", err)
-	} else if len(metadataBytes) != int(metadataSize) {
-		return nil, fmt.Errorf("read metadata: short read")
-	}
-	defer s.Discard(int(metadataSize))
-
 	var metadata streamsmd.StreamMetadata
-	if err := proto.Unmarshal(metadataBytes, &metadata); err != nil {
-		return nil, fmt.Errorf("unmarshal metadata: %w", err)
+	if err := scanProto(s, &metadata); err != nil {
+		return nil, fmt.Errorf("stream metadata: %w", err)
 	}
 	return &metadata, nil
 }
 
-// scanColumnMetadata decodes a set of Pages from s.
+// scanColumnMetadata scans a column's metadata from s.
 func scanColumnMetadata(s scanner.Scanner) (*streamsmd.ColumnMetadata, error) {
-	metadataSize, err := binary.ReadUvarint(s)
-	if err != nil {
-		return nil, fmt.Errorf("read metadata size: %w", err)
-	}
-
-	metadataBytes, err := s.Peek(int(metadataSize))
-	if err != nil {
-		return nil, fmt.Errorf("read metadata: %w", err)
-	} else if len(metadataBytes) != int(metadataSize) {
-		return nil, fmt.Errorf("read metadata: short read")
-	}
-	defer s.Discard(int(metadataSize))
-
 	var metadata streamsmd.ColumnMetadata
-	if err := proto.Unmarshal(metadataBytes, &metadata); err != nil {
-		return nil, fmt.Errorf("unmarshal metadata: %w", err)
+	if err := scanProto(s, &metadata); err != nil {
+		return nil, fmt.Errorf("column metadata: %w", err)
 	}
 	return &metadata, nil
+}
+
+// scanProto scans a proto message from s and stores it in pb. Proto messages
+// are expected to be encoded with their size, followed by the proto bytes.
+func scanProto(s scanner.Scanner, pb proto.Message) error {
+	// TODO(rfratto): Should we use a pool for the protoBytes buffer here?
+
+	size, err := binary.ReadUvarint(s)
+	if err != nil {
+		return fmt.Errorf("read size: %w", err)
+	}
+	protoBytes := make([]byte, size)
+	if _, err := io.ReadFull(s, protoBytes); err != nil {
+		return fmt.Errorf("read message: %w", err)
+	}
+	if err := proto.Unmarshal(protoBytes, pb); err != nil {
+		return fmt.Errorf("unmarshal message: %w", err)
+	}
+	return nil
 }
